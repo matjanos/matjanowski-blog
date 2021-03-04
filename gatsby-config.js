86cb823349f808fe5149291cc266ad8c64118ee8
Jakub Matjanowski <jakub@greenpool.no>
2021-03-04 21:20:44 +0100

    create site map

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-sharp`,
     {
       resolve: "gatsby-plugin-google-tagmanager",
