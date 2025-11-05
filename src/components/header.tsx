a8dfd834343ff2bbc93b538369553399608fadfb
Kuba Matjanowski <k.matjanowski@gmail.com>
2025-11-05 23:21:48 +0100

    Polish header tooltip styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,12 +18,30 @@ const Header = () => {
   `)
 
 const name = data.site.siteMetadata.author.name;
+const [firstName, ...restNameParts] = name.split(" ")
+const remainingName = restNameParts.join(" ")
 
     return (<div style={{textAlign: 'center'}}>
         <Avatar />
-        <div>Hi, my name is</div>
-        <div className='author-name-header'>{name}</div>
-        <div>and I'm really glad that you are here!</div>
+        <div className='author-greeting'>Hi, my name is</div>
+        <div className='author-name-header'>
+            <span className="name-highlight">
+                {firstName}
+                <span
+                    className="name-tooltip"
+                    tabIndex={0}
+                    aria-label="Jakub, pronounced ya-koob, is the formal name behind Kuba"
+                >
+                    <span aria-hidden="true" className="name-tooltip-icon">✻</span>
+                    <span className="name-tooltip-bubble" role="tooltip">
+                        <span className="name-tooltip-term">Jakub <em className="name-tooltip-phonetic">/ˈja.kup/</em></span>
+                        <span className="name-tooltip-definition">Polish form of <strong>Jacob</strong>. "Kuba" is the everyday short form, but official documents still say Jakub.</span>
+                    </span>
+                </span>
+            </span>
+            {remainingName ? ` ${remainingName}` : ""}
+        </div>
+        <div className='author-tagline'>and I'm really glad that you are here!</div>
     </div>)
 }
 
